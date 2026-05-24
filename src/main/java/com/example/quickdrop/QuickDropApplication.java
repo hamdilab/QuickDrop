490c90d5247c9a751dcd3a9193546aeb659d0ccf
Mohamed Aziz Hamdi <azizhamdi475@gmail.com>
2026-05-24 16:28:33 +0100

    Initial Spring Boot project structure

---
+++ src/main/java/com/example/quickdrop/QuickDropApplication.java
@@ -0,0 +1,13 @@
+package com.example.quickdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickDropApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickDropApplication.class, args);
+    }
+
+}
